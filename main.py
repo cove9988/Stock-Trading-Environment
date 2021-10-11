4c8e63af8f832bf033ee16b944b848fcc0d2a414
paulg <cove9988@gmail.com>
2021-10-12 10:15:20 +1100

    use latest stabe_baseline3 PPO

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-import gym
-import json
-import datetime as dt
-
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import PPO2
+from stable_baselines3.ppo import MlpPolicy
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3 import PPO
 
 from env.StockTradingEnv import StockTradingEnv
 
 import pandas as pd
 
-df = pd.read_csv('./data/AAPL.csv')
+df = pd.read_csv('./data/MSFT.csv')
 df = df.sort_values('Date')
 
 # The algorithms require a vectorized environment to run
 env = DummyVecEnv([lambda: StockTradingEnv(df)])
 
-model = PPO2(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=20000)
+model = PPO(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=50)
 
 obs = env.reset()
-for i in range(2000):
+
+for i in range(len(df['Date'])):
     action, _states = model.predict(obs)
     obs, rewards, done, info = env.step(action)
-    env.render()
+    env.render(mode='live')
